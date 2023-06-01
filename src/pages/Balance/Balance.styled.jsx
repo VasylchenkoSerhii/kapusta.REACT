a6e70fa512d73415ca0a5e62615a8e0e980b16da
Stanislav2705 <stasrogal25@gmail.com>
2023-06-01 23:49:19 +0200

    category input decktop

--- src/pages/Balance/Balance.styled.jsx
+++ src/pages/Balance/Balance.styled.jsx
@@ -5,6 +5,7 @@ import bgDesc from '../../images/cabagge/bg-desc-home.png';
 import MessageBg from '../../images/message-bg.png';
 import { ImCalendar } from 'react-icons/im';
 import { FaCalculator } from 'react-icons/fa';
+import { BsChevronDown, BsChevronUp } from 'react-icons/bs';
 import { ErrorMessage, Field, Form } from 'formik';
 
 export const Section = styled.section`
@@ -337,13 +338,14 @@ export const BalockCalendar = styled.div`
   margin-left: 40px;
   /* margin: 0; */
   padding: 0;
-  width: 120px;
+  width: 100px;
+  @media screen and (min-width: 1200px) {
+    margin-left: 32px;
+  }
 `
 export const ButtonCalendar = styled.div`
   display: flex;
   align-items: center;
-  margin-right: 44px;
-  width: 120px;
   height: 46px;
 `
 export const CalendarImage = styled(ImCalendar)`
@@ -369,26 +371,51 @@ export const ViewCalendar = styled.div`
 export const BlockProduct = styled.div`
   margin-top: 26px;
   margin-left: 44px;
+  @media screen and (min-width: 1200px) {
+    margin-left: 32px;
+  }
 `
-export const ProductForm = styled.div``
-export const MainForm = styled(Form)``
+export const ProductForm = styled.div`
+   @media screen and (min-width: 1200px) {
+    width: 898px;
+  }
+`
+export const MainForm = styled(Form)`
+  position: relative;
+  @media screen and (min-width: 1200px) {
+    display: flex;
+    align-items: center;
+
+  }
+`
 export const ProductContainer = styled.div`
   display: flex;
   width: 480px;
   height: 44px;
   border: 2px solid ${p => p.theme.colors.background};;
   border-radius: 16px 16px 16px 0px;
+  @media screen and (min-width: 1200px) {
+    width: 578px;
+  }
 `
 export const ProductInput = styled(Field)`
   border: none;
   height: 40px;
   width: 160px;
   margin-left: 20px;
+  outline: none;
   font-family: ${p => p.theme.fonts.body};
   font-weight: ${p => p.theme.fontWeights.normal};
   font-size: ${p => p.theme.fontSizes.s};
   line-height: 1,17;
   letter-spacing: 0.02em;
+  @media screen and (min-width: 1200px) {
+    width: 290px;
+  }
+`
+export const CategoryContainer = styled.div`
+  position: relative;
+  width: 186px;
 `
 export const CategoryInput = styled(Field)`
   width: 186px;
@@ -397,6 +424,7 @@ export const CategoryInput = styled(Field)`
   padding-right: 60px;
   border-top: none;
   border-bottom: none;
+  outline: none;
   border-left:2px solid ${p => p.theme.colors.background};
   border-right:2px solid ${p => p.theme.colors.background};
   border-radius: 0px;
@@ -405,11 +433,61 @@ export const CategoryInput = styled(Field)`
   font-size: ${p => p.theme.fontSizes.s};
   line-height: 1,17;
   letter-spacing: 0.02em;
+  @media screen and (min-width: 1200px) {
+    width: 169px;
+  }
 `
-export const CategoryList = styled.ul``
+export const CategoryImageDown = styled(BsChevronDown)`
+  position: absolute;
+  left: 156px;
+  top: 13px;
+  width: 15px;
+  height: 15px;
+  @media screen and (min-width: 1200px) {
+    left: 140px;
+  }
+`
+export const CategoryImageUp = styled(BsChevronUp)`
+  position: absolute;
+  left: 156px;
+  top: 13px;
+  width: 15px;
+  height: 15px;
+  @media screen and (min-width: 1200px) {
+    left: 140px;
+  }
+`
+export const CategoryList = styled.ul`
+  position: absolute;
+  width: 185px;
+  top: 41px;
+  list-style: none;
+  background: #FFFFFF;
+  border: none;
+  box-shadow: 0px 3px 4px rgba(170, 178, 197, 0.4);
+`
+export const CategoryItem = styled.li`
+  display: flex;
+  align-items: center;
+  padding: 0px 20px 0px 20px;
+  height: 32px;
+  font-family: ${p => p.theme.fonts.body};
+  font-weight: ${p => p.theme.fontWeights.normal};
+  font-size: ${p => p.theme.fontSizes.s};
+  line-height: 1,17;
+  letter-spacing: 0.02em;
+  color: #C7CCDC;
+  &:active {
+    background-color: ${p => p.theme.colors.background};
+    color: #52555F;
+    }
+`
 export const CalculatorContainer = styled.div`
  display: flex;
  align-items: center;
+ @media screen and (min-width: 1200px) {
+    padding-right: 12px;
+  }
 `
 export const CalculateInput = styled(Field)`
   margin-left: 20px;
@@ -417,6 +495,7 @@ export const CalculateInput = styled(Field)`
   width: 60px;
   height: 40px;
   border: none;
+  outline: none;
   font-family: ${p => p.theme.fonts.body};
   font-weight: ${p => p.theme.fontWeights.normal};
   font-size: ${p => p.theme.fontSizes.s};
@@ -444,5 +523,60 @@ export const Error = styled(ErrorMessage)`
     font-size: ${p => p.theme.fontSizes.s};
   }
 `;
-export const InputBtn = styled.button``
-export const ClearBtn = styled.button``
+export const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 205px;
+  margin-top: 32px;
+  @media screen and (min-width: 1200px) {
+    margin-top: 0;
+    margin-left: 32px;
+  }
+`
+export const InputBtn = styled.button`
+  width: 125px;
+  height: 44px;
+  padding: 12px 34px 12px 34px;
+  margin-right: 15px;
+  border-radius: 16px;
+  font-family: ${p => p.theme.fonts.body};
+  font-weight: ${p => p.theme.fontWeights.bold};
+  font-size: ${p => p.theme.fontSizes.s};
+  line-height: 1,17;
+  letter-spacing: 0.02em;
+  text-transform: uppercase;
+  border: 2px solid ${p => p.theme.colors.background};
+  color: ${p => p.theme.colors.text};
+  background-color: ${p => p.theme.colors.white};
+  &:active {
+    border: none;
+    background-color: ${p => p.theme.colors.accent};
+    color:${p => p.theme.colors.white};
+  }
+  @media screen and (min-width: 1200px) {
+    width: 136px;
+  }
+`
+export const ClearBtn = styled.button`
+  width: 125px;
+  height: 44px;
+  padding: 12px 34px 12px 34px;
+  border-radius: 16px;
+  font-family: ${p => p.theme.fonts.body};
+  font-weight: ${p => p.theme.fontWeights.bold};
+  font-size: ${p => p.theme.fontSizes.s};
+  line-height: 1,17;
+  letter-spacing: 0.02em;
+  text-transform: uppercase;
+  border: 2px solid ${p => p.theme.colors.background};
+  color: ${p => p.theme.colors.text};
+  background-color: ${p => p.theme.colors.white};
+  &:active {
+    border: none;
+    background-color: ${p => p.theme.colors.accent};
+    color:${p => p.theme.colors.white};
+  }
+  @media screen and (min-width: 1200px) {
+    width: 136px;
+  }
+`
