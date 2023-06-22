c44dc3978eeff924badfd581832e64d5e49de047
Vasylchenko Serhii <98521667+VasylchenkoSerhii@users.noreply.github.com>
2023-06-22 21:56:07 +0300

    StylingForBalanceSummary
    Completed Styling for BalanceSummary Component

--- src/components/BalanceSummary/BalanceSummary.styled.jsx
+++ src/components/BalanceSummary/BalanceSummary.styled.jsx
@@ -1,14 +1,91 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Section = styled.section`
   margin-top: 16px;
   margin-bottom: 16px;
+  padding: 0px 20px;
+
+  @media screen and (min-width: 768px) {
+    padding: 0px 32px;
+  }
+
+  @media screen and (min-width: 1200px) {
+    padding: 0px 123px;
+  }
+`;
+
+export const WrapperForTabletGroup = styled.div`
+  @media screen and (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 32px;
+  }
+`;
+
+export const WrapperBackLink = styled.div`
+  @media screen and (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    text-align: center;
+  }
+`;
+
+export const BalanceGroupWrapper = styled.div`
+  @media screen and (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    text-align: center;
+  }
+`;
+
+export const WrapperPeriodAndBalance = styled.div`
+  @media screen and (min-width: 768px) {
+    display: flex;
+
+    > div:nth-child(1) {
+      order: 2;
+    }
+
+    & > div:nth-child(2) {
+      order: 1;
+    }
+  }
+`;
+
+export const BackLink = styled(Link)`
+  margin-bottom: 16px;
+  text-align: center;
+  @media screen and (min-width: 768px) {
+    margin-bottom: 0px;
+    margin-right: 15px;
+  }
+`;
+
+export const BackLinkIcon = styled.svg`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const BackLinkText = styled.p`
+  @media screen and (max-width: 767px) {
+    display: none;
+  }
 `;
 
 export const WrapperPeriod = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-bottom: 32px;
+
+  @media screen and (min-width: 767px) {
+    margin-bottom: 0px;
+  }
 `;
 
 export const PeriodSubheading = styled.p`
@@ -23,6 +100,11 @@ export const BalanceSubheading = styled.p`
   text-align: center;
   font-weight: ${p => p.theme.fontWeights.medium};
   font-size: ${p => p.theme.fontSizes.m};
+
+  @media screen and (min-width: 767px) {
+    margin-bottom: 0px;
+    margin-right: 20px;
+  }
 `;
 
 export const PeriodContainer = styled.div`
@@ -51,7 +133,7 @@ export const BalanceWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 183px;
+  width: 125px;
   height: 44px;
   margin-left: auto;
   margin-right: auto;
@@ -59,6 +141,15 @@ export const BalanceWrapper = styled.div`
   text-align: center;
   border: 2px solid #000000;
   border-radius: 22px;
+
+  @media screen and (min-width: 768px) {
+    margin-right: 102px;
+    margin-bottom: 0px;
+  }
+
+  @media screen and (min-width: 1200px) {
+    margin-right: 280px;
+  }
 `;
 
 export const BalanceValye = styled.span`
@@ -68,31 +159,43 @@ export const BalanceValye = styled.span`
 `;
 
 export const ExpensesIncomeList = styled.ul`
+  position: relative;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
-  width: 300px;
+  min-width: 280px;
   height: 85px;
-  margin-left: auto;
-  margin-right: auto;
   padding: 10px 20px 25px;
   list-style: none;
   background: #ffffff;
   box-shadow: 5px 10px 20px rgba(170, 178, 197, 0.4);
   border-radius: 20px;
+
+  @media screen and (min-width: 768px) {
+    min-width: 704px;
+    height: 50px;
+    padding: 15px 0px;
+  }
 `;
 
 export const ExpensesIncomeItem = styled.li`
-  position: relative;
   &:not(:last-child)::after {
     content: '';
     position: absolute;
     width: 0px;
     height: 70px;
-    left: 127px;
-    top: -11px;
+    left: 49.5%;
+    top: 6px;
 
     border: 1px solid #e0e5eb;
+  }
+
+  @media screen and (min-width: 768px) {
+    display: flex;
+
+    &:not(:last-child)::after {
+      height: 36px;
+    }
   }
 `;
 
@@ -101,6 +204,10 @@ export const ExpensesIncome = styled.p`
   text-align: center;
   font-size: ${p => p.theme.fontSizes.m};
   font-weight: ${p => p.theme.fontWeights.bold};
+
+  @media screen and (min-width: 768px) {
+    margin-right: 15px;
+  }
 `;
 
 export const Expenses = styled.p`
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,9 +3,9 @@ import AppBar from 'components/AppBar/AppBar';
 
 export default function Layout() {
   return (
-    <main>
+    <div>
       <AppBar />
       <Outlet />
-    </main>
+    </div>
   );
 }
