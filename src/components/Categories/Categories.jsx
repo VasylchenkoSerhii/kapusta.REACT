f1216c866f19c5cbc34d96c2e965e5e1c1f91d01
Vasylchenko Serhii <98521667+VasylchenkoSerhii@users.noreply.github.com>
2023-07-10 22:11:07 +0300

    completed task

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Sprite from '../../images/currentPeriod.svg';
 import {
   BackgroundIcon,
@@ -29,36 +30,72 @@ export default function Categories() {
     'Other',
   ];
 
+  const income = ['salary', 'other'];
+
+  const [activeCategory, setActiveCategory] = useState('expenses');
+
+  const handleExpensesClick = () => {
+    setActiveCategory('expenses');
+  };
+
+  const handleIncomeClick = () => {
+    setActiveCategory('income');
+  };
+
   return (
     <Section>
       <MainWrapper>
         <WrapperExpenses>
-          <ExpensesBtn type='button'>
+          <ExpensesBtn
+            type='button'
+            onClick={handleExpensesClick}
+            disabled={activeCategory === 'expenses'}
+          >
             <svg width={6} height={15}>
               <use href={`${Sprite}#icon-arrow-left`}></use>
             </svg>
           </ExpensesBtn>
-          <ExpensesText>Expenses</ExpensesText>
-          <ExpensesBtn type='button'>
+          <ExpensesText>
+            {activeCategory === 'expenses' ? 'Expenses' : 'Income'}
+          </ExpensesText>
+          <ExpensesBtn
+            type='button'
+            onClick={handleIncomeClick}
+            disabled={activeCategory === 'income'}
+          >
             <svg width={6} height={15}>
               <use href={`${Sprite}#icon-arrow-right`}></use>
             </svg>
           </ExpensesBtn>
         </WrapperExpenses>
         <CategoriesList>
-          {categories.map((el, index) => (
-            <CategoriesItem key={index}>
-              <CategoriesSum>5 000.00</CategoriesSum>
-              <CategoriesBtn type='button'>
-                <BackgroundIcon>
-                  <CategoriesIcon width={56} height={56}>
-                    <use href={`${Sprite}#${el.toLowerCase()}`}></use>
-                  </CategoriesIcon>
-                </BackgroundIcon>
-              </CategoriesBtn>
-              <CategoriesName>{el}</CategoriesName>
-            </CategoriesItem>
-          ))}
+          {activeCategory === 'expenses'
+            ? categories.map((el, index) => (
+                <CategoriesItem key={index}>
+                  <CategoriesSum>5 000.00</CategoriesSum>
+                  <CategoriesBtn type='button'>
+                    <BackgroundIcon>
+                      <CategoriesIcon width={56} height={56}>
+                        <use href={`${Sprite}#${el.toLowerCase()}`}></use>
+                      </CategoriesIcon>
+                    </BackgroundIcon>
+                  </CategoriesBtn>
+                  <CategoriesName>{el}</CategoriesName>
+                </CategoriesItem>
+              ))
+            : income.map((el, index) => (
+                <CategoriesItem key={index}>
+                  <CategoriesSum>5 000.00</CategoriesSum>
+                  <CategoriesBtn type='button'>
+                    <BackgroundIcon>
+                      <CategoriesIcon width={56} height={56}>
+                        <use href={`${Sprite}#${el.toLowerCase()}`}></use>
+                      </CategoriesIcon>
+                    </BackgroundIcon>
+                  </CategoriesBtn>
+                  <CategoriesName>{el}</CategoriesName>
+                </CategoriesItem>
+              ))}
         </CategoriesList>
       </MainWrapper>
     </Section>
