f2845055c644ddc53c32b402aa5e2cf9a2ac32f9
Vasylchenko Serhii <98521667+VasylchenkoSerhii@users.noreply.github.com>
2023-07-21 23:25:33 +0300

    start work on the component

--- src/components/ExpenseChar/ExpenseChar.jsx
+++ src/components/ExpenseChar/ExpenseChar.jsx
@@ -1,5 +1,74 @@
 import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
-export default function ExpenseChar() {
-  return <div></div>;
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default function ExpenseChar({ transactions }) {
+  const descriptions = transactions.map(transaction => transaction.description);
+  const sums = transactions.map(transaction => transaction.sum);
+
+  const barData = {
+    labels: descriptions,
+    datasets: [
+      {
+        label: 'Sum',
+        data: sums,
+        backgroundColor: 'rgba(54, 162, 235, 0.5)',
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      // Додаємо налаштування плагіну datalabels для відображення суми над стовпчиками
+      datalabels: {
+        anchor: 'end',
+        align: 'top',
+        color: 'black',
+        font: {
+          weight: 'bold',
+        },
+        formatter: value => `${value} UAH`,
+      },
+    },
+    scales: {
+      x: {
+        type: 'category',
+        beginAtZero: true,
+      },
+      y: {
+        beginAtZero: true,
+        grid: {
+          display: true,
+          offset: true,
+          drawBorder: false,
+        },
+        ticks: {
+          display: false,
+        },
+      },
+    },
+  };
+
+  return <Bar data={barData} options={options} />;
 }
