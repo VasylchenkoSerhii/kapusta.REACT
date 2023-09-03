820651fc858655508fed45953460c5bfed90d7b0
Vasylchenko Serhii <98521667+VasylchenkoSerhii@users.noreply.github.com>
2023-09-03 13:14:21 +0300

    comlited toast

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,25 @@
 import { Outlet } from 'react-router-dom';
 import AppBar from 'components/AppBar/AppBar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Layout() {
   return (
     <div>
       <AppBar />
       <Outlet />
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='colored'
+      />
     </div>
   );
 }
